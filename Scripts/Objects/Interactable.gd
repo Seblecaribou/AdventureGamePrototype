c850207fccbe8dced2bf92339b7860f8206bb8cb
Sebastien Morin <morin.seb.89@gmail.com>
2024-03-15 23:29:30 +0100

    Revert to one interactable scene type

--- Scripts/Objects/Interactable.gd
+++ Scripts/Objects/Interactable.gd
@@ -1,24 +1,21 @@
 class_name Interactable
 extends Area2D
 
-@export var item_id : String
-var item_data : Node
-var character_data : Node
-var interact_type : String
+@export var interactable_id : String
+var iteractable_data : Node
+var interactable_type : String
 var interact_label : String
 
 func _ready():
-	interact_type = item_id.get_slice("_", 0)
-	item_data = get_node("./ItemData")
-	character_data = get_node("./CharacterData")
-	if item_data != null:
+	iteractable_data = get_node("./ItemData")
+	if iteractable_data != null:
 		configure_interactable()
 
 
 func configure_interactable():
-	interact_type = item_id.get_slice("_", 0)
-	item_data.load_item_data(item_id, interact_type)
-	match interact_type:
+	interactable_type = interactable_id.get_slice("_", 0)
+	iteractable_data.load_interactable_data(interactable_id, interactable_type)
+	match interactable_type:
 		"obs":
 			interact_label = "Examine"
 		"pic":
--- Scripts/Resources/Item.gd
+++ Scripts/Resources/Item.gd
@@ -1,15 +1,31 @@
-class_name Character
+class_name InteractableData
 extends Node
 
-var character_name : String
-var character_label : String
-var character_starting_room_id : String
-var character_description : String
-var character_sprite_id : String
+var interactable_name : String
+var interactable_label : String
+var interactable_starting_room_id : String
+var interactable_description : String
+var interactable_sprite_id : String
 
-func load_item_data(id : String):
+func load_interactable_data(id : String, interactable_type : String):
 	if id:
-		pass
+		match interactable_type:
+			"obs":
+				interactable_name = StaticDataSingleton.observable_items[id].name
+				interactable_starting_room_id = StaticDataSingleton.observable_items[id].room_id
+				interactable_description = StaticDataSingleton.observable_items[id].description
+				interactable_label = StaticDataSingleton.observable_items[id].label
+				interactable_sprite_id = StaticDataSingleton.observable_items[id].sprite_id				
+			"pic":
+				interactable_name = StaticDataSingleton.pickable_items[id].name
+				interactable_starting_room_id = StaticDataSingleton.pickable_items[id].room_id				
+				interactable_description = StaticDataSingleton.pickable_items[id].description
+				interactable_label = StaticDataSingleton.pickable_items[id].label
+				interactable_sprite_id = StaticDataSingleton.pickable_items[id].sprite_id
+			"act":
+				pass
+			"char":
+				pass
 	else:
-		print("Error while loading the character data: no character_id was provided.")
-	print("Item data loaded: " + id)
+		print("Error while loading the interactable data: no interactable_id was provided.")
+	print("Interactable data loaded: " + id)
