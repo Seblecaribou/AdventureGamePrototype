394084a343de5dec4328333eb28df63bf2ef61f0
Seblecaribou <65242707+Seblecaribou@users.noreply.github.com>
2024-04-01 13:12:42 +0200

    LaptopAsus - added details to DialogueManager and Component

--- Scripts/Components/DialogueComponent.gd
+++ Scripts/Components/DialogueComponent.gd
@@ -5,3 +5,17 @@ extends Node2D
 @export var dialogue_text : RichTextLabel
 
 #TODO take string to display, animate and change them
+## Displays full dialogue
+func display_dialogue(dialogue: String) -> void:
+	type_dialogue(dialogue)
+
+## Displays dialogue following a typewriter animation
+func type_dialogue(dialogue : String) -> void:
+	dialogue_text.visible = 0.0
+	dialogue_text.text = dialogue
+	var tween: Tween = create_tween()
+	tween.tween_property(dialogue_text, "visible_ratio", 1.0, 2.0).from(0.0)
+
+## Enables/disables BBcode animations
+func manage_bbcode(isEnabled : bool) -> void:
+	dialogue_text.bbcode_enabled = isEnabled
--- Scripts/Singletons/DialogueManager.gd
+++ Scripts/Singletons/DialogueManager.gd
@@ -3,6 +3,7 @@ extends Node
 
 @export var dialogue_data : DialogueData
 @export var dialogue_component : DialogueComponent
+@export var player_character : PlayerCharacter
 var current_quests_steps : Array[String]
 var current_dialogue : String
 var current_character : Interactable
@@ -14,14 +15,19 @@ func _ready():
 		configure_dialogue()
 
 
-func _on_player_character():
-	#TODO create a signal that can be listened to and that changes the content of current_quests_steps
+#TODO create a signal that can be listened to and that changes the content of current_quests_steps
+func _on_player_character_interacted():
+	if dialogue_data != null:
+		configure_dialogue()
+	dialogue_component.visible = true
 	pass
 
 func display_dialogue() -> void:
-	#TODO make dialogue appear in DialogueFrame if Interaction calls for it
-	# 1 - Put text from current_dialogue into dialogue_component.dialogue_text
-	pass
+	#TODO make dialogue appear in DialogueFrame if Interaction calls it
+	dialogue_component.type_dialogue(current_dialogue)
+	
+func hide_dialogue() -> void:
+	dialogue_component.visible = false
 
 func load_dialogue() -> void:
 	#TODO loads dialogue based of current_quests_steps and current_character
