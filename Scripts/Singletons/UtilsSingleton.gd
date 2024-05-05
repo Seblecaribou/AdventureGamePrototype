a76513aff4483bbf141f878dbe06233c3591cf9a
Sebastien Morin <morin.seb.89@gmail.com>
2024-05-05 15:14:46 +0200

    LaptopAsus - Added a save method

--- Scripts/Singletons/UtilsSingleton.gd
+++ Scripts/Singletons/UtilsSingleton.gd
@@ -10,4 +10,14 @@ func load_json_file(filepath : String):
 		else: 
 			print("An error occured while reading the file. Check the file format.")
 	else: 
-		print("File does not exists. Check either the filepath or the file.")
+		print("File does not exists. Check either the filepath or the file at " + filepath)
+
+
+func save_json_file(file_name : String, data_to_parse):
+	var json = JSON.new()
+	var file_path : String = "user://" + file_name + ".json"
+
+	var save_file = FileAccess.open(file_path, FileAccess.WRITE)
+	save_file.store_string(json.stringify(data_to_parse))
+	save_file.close()
+	save_file = null
