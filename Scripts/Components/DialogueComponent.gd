ea808c927a0f84cd12a9582bc175672ee5219413
Seblecaribou <65242707+Seblecaribou@users.noreply.github.com>
2024-06-26 21:58:41 +0200

    AsusLaptop - Finished implementing the retrieval of dialogues + revamped the typewriter effect

--- Scripts/Components/DialogueComponent.gd
+++ Scripts/Components/DialogueComponent.gd
@@ -5,8 +5,25 @@ extends Node2D
 @export var dialogue_text : RichTextLabel
 @export var portrait_right : Sprite2D
 @export var portrait_left : Sprite2D
+var full_text: String = ""
+var current_char_index: int = 0
+var typing_timer: Timer = null
 
-#TODO take string to display, animate and change them
+func _ready():
+	typing_timer = Timer.new()
+	typing_timer.set_wait_time(0.05)  # Adjust the wait time to control typing speed
+	typing_timer.set_one_shot(false)
+	typing_timer.timeout.connect(_on_timer_timeout)
+	add_child(typing_timer)
+
+
+func _on_timer_timeout() -> void:
+	if current_char_index < full_text.length():
+		dialogue_text.text += full_text[current_char_index]
+		current_char_index += 1
+	else:
+		typing_timer.stop()
+
 ## Displays full dialogue
 func display_dialogue(dialogue: String) -> void:
 	type_dialogue(dialogue)
@@ -14,10 +31,10 @@ func display_dialogue(dialogue: String) -> void:
 
 ## Displays dialogue following a typewriter animation
 func type_dialogue(dialogue : String) -> void:
-	dialogue_text.visible = 0.0
-	dialogue_text.text = dialogue
-	var tween: Tween = create_tween()
-	tween.tween_property(dialogue_text, "visible_ratio", 1.0, 2.0).from(0.0)
+	full_text = dialogue
+	current_char_index = 0
+	dialogue_text.text = ""
+	typing_timer.start()
 
 ## Enables/disables BBcode animations
 func manage_bbcode(isEnabled : bool) -> void:
--- Scripts/Managers/DialogueManager.gd
+++ Scripts/Managers/DialogueManager.gd
@@ -5,7 +5,7 @@ extends Node
 @export var dialogue_data : DialogueData
 @export var dialogue_component : DialogueComponent
 var current_quests_objectives : Array[String]
-var current_dialogues : Array[String]
+var current_dialogues : Array[DialogueData.DialogueObjectiveData]
 
 
 func _ready():
@@ -14,32 +14,30 @@ func _ready():
 	#TODO get the current quest step from the quest manager signal
 	
 
-func display_dialogue() -> void:
-	#TODO make dialogue appear in DialogueFrame if Interaction calls it
-	for dialogue in current_dialogues:
-		dialogue_component.type_dialogue(dialogue)
-	
 func hide_dialogue() -> void:
 	dialogue_component.visible = false
 
 func configure_dialogue(interactable : ):
-	#TODO loads dialogues based of current_quests_steps and current_character
 	for objective in current_quests_objectives:
 		var quest_id = objective.get_slice("_", 0)
 		var step_id = objective.get_slice("_", 1)
 		var objective_id = objective.get_slice("_", 2)
 		for dialogue in dialogue_data.dialogues:
-			pass
-		pass 
-	pass
+			if dialogue.quest_id == quest_id:
+				for dialogue_step in dialogue.steps:
+					if dialogue_step.step_id == step_id:
+						for dialogue_objective in dialogue_step.objectives:
+							if dialogue_objective.objective_id == objective_id:
+								current_dialogues.append(dialogue_objective)
 
-
-#Signal callback functions
+#Signals callback functions
 func on_player_character_interacted(emitter : Node, interactable : Interactable, interaction_type : String):
 	if interaction_type == "char":
 		dialogue_data.load_dialogue_data(interactable)
 	configure_dialogue(interactable)
-	#dialogue_component.display_dialogue()
+	for dialogue in current_dialogues:
+		for content in dialogue.content:
+			dialogue_component.display_dialogue(content)
 
 func on_update_all_quests(emitter : Node, active_quests : Array[QuestData], FinishedQuests : Array[QuestData]):
 	#TODO implement recuperation of data active/finished quest
