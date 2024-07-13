51764d3d424a846ea2c47181a8747850698633c2
Seblecaribou <65242707+Seblecaribou@users.noreply.github.com>
2024-07-13 22:50:31 +0200

    AsusLaptop - Tweaking the button component

--- Scripts/Menus/Dialogue/DialogueButtonComponent.gd
+++ Scripts/Menus/Dialogue/DialogueButtonComponent.gd
@@ -4,4 +4,7 @@ extends Button
 var index : int
 var objective_id : String
 
+func _ready():
+	visible = false
 #TODO Add an override signal "pressed" to send "self" in it
+#TODO Load texture
--- Scripts/Menus/Dialogue/DialogueMenu.gd
+++ Scripts/Menus/Dialogue/DialogueMenu.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 
 func add_button(button_index : int, objective_id : String, label : String):
-	var button_scene = preload("res://Scenes/DialogueButtonComponent.tscn")
+	var button_scene = preload("res://Scenes/DialogueButton.tscn")
 	var new_button_instance = button_scene.instantiate()
 	new_button_instance.index = button_index
 	new_button_instance.objective_id = objective_id
