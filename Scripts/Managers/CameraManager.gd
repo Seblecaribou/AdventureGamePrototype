8257b55c4e8378061199401872796805b35213d1
Sebastien Morin <morin.seb.89@gmail.com>
2025-02-08 13:38:36 +0100

    Merge branch 'feature/camera-bounderies'

--- Scripts/Managers/CameraManager.gd
+++ Scripts/Managers/CameraManager.gd
@@ -1,9 +1,10 @@
 class_name CameraManager
 extends Camera2D
 
+var current_room : RoomComponent
+
 func _ready() -> void:
 	self.global_position.y += AppSettingsSingleton.camera_y_offset
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	self.global_position.x = %PlayerCharacter.global_position.x
-	
