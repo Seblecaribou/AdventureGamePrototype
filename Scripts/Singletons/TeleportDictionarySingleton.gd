a0a426df3706bcbb2bce515d8b237871b56aac71
Sebastien Morin <morin.seb.89@gmail.com>
2025-03-05 22:27:16 +0100

    Desktop - Added balcony 101 + fixed camera offset

--- Scripts/Singletons/TeleportDictionarySingleton.gd
+++ Scripts/Singletons/TeleportDictionarySingleton.gd
@@ -5,30 +5,30 @@ extends Node
 #Corridor
 var hotel_corridor_01 : Dictionary = {
 	collision_mask = 2,
-	zoom_mutiplier = Vector2(1.10,1.10),
+	zoom_mutiplier = Vector2(1.38,1.38),
 	zoom_delay = .15
 	}
 	
 var hotel_corridor_02 : Dictionary = {
 	collision_mask = 2,
-	zoom_mutiplier = Vector2(1.10,1.10),
+	zoom_mutiplier = Vector2(1.38,1.38),
 	zoom_delay = .15
 	}
 
 #Room100
 var room_100_01 : Dictionary = {
 	collision_mask = 2,
-	zoom_mutiplier = Vector2(1,1),
+	zoom_mutiplier =  Vector2(1.38,1.38),
 	zoom_delay = .15
 	}
 
 var room_100_02 : Dictionary = {
 	collision_mask = 2,
-	zoom_mutiplier = Vector2(1.10,1.10),
+	zoom_mutiplier =  Vector2(1.38,1.38),
 	zoom_delay = .15
 	}
 	
-var balcony_100_01 : Dictionary = {
+var balcony_100 : Dictionary = {
 	collision_mask = 3,
 	zoom_mutiplier = Vector2(1.15,1.15),
 	zoom_delay = .25
@@ -37,14 +37,20 @@ var balcony_100_01 : Dictionary = {
 #Room101
 var room_101_01 : Dictionary = {
 	collision_mask = 2,
-	zoom_mutiplier = Vector2(1,1),
+	zoom_mutiplier = Vector2(1.38,1.38),
 	zoom_delay = .15
 	}
 
 var room_101_02 : Dictionary = {
 	collision_mask = 2,
-	zoom_mutiplier = Vector2(1.10,1.10),
+	zoom_mutiplier = Vector2(1.38,1.38),
 	zoom_delay = .15
+	}
+	
+var balcony_101 : Dictionary = {
+	collision_mask = 3,
+	zoom_mutiplier = Vector2(1.30,1.30),
+	zoom_delay = .25
 	}
 
 #Reception
@@ -64,6 +70,6 @@ var reception_02 : Dictionary = {
 #STREET
 var street_01 : Dictionary = {
 	collision_mask = 2,
-	zoom_mutiplier = Vector2(0.8,0.8),
+	zoom_mutiplier = Vector2(1,1),
 	zoom_delay = .5
 	}
